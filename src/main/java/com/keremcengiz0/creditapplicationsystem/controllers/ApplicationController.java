60cb9bf874b3ac8e364566864ef76de9d1701fa0
keremcengiz0 <keremcengiz0@hotmail.com>
2023-02-07 18:27:31 +0300

    added ApplicationController and query for findCustomerByApplicationId method in CustomerRepository.

--- src/main/java/com/keremcengiz0/creditapplicationsystem/controllers/ApplicationController.java
+++ src/main/java/com/keremcengiz0/creditapplicationsystem/controllers/ApplicationController.java
@@ -1,4 +1,41 @@
 package com.keremcengiz0.creditapplicationsystem.controllers;
 
+import com.keremcengiz0.creditapplicationsystem.CreditApplicationSystemApplication;
+import com.keremcengiz0.creditapplicationsystem.dtos.ApplicationDTO;
+import com.keremcengiz0.creditapplicationsystem.services.abstracts.ApplicationService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/applications")
 public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    @Autowired
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ApplicationDTO>> getAll() {
+        log.info("ApplicationController: A request has been received to list all applications.");
+        return new ResponseEntity<>(this.applicationService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/get-status/{identityNumber}")
+    public ResponseEntity<ApplicationDTO> getStatus(@PathVariable(value = "identityNumber") String identityNumber) {
+        log.info("ApplicationController: A request has been received to list one application.");
+        return new ResponseEntity<>(this.applicationService.getStatus(identityNumber), HttpStatus.OK);
+    }
 }
--- src/main/java/com/keremcengiz0/creditapplicationsystem/repositories/CustomerRepository.java
+++ src/main/java/com/keremcengiz0/creditapplicationsystem/repositories/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.keremcengiz0.creditapplicationsystem.repositories;
 import com.keremcengiz0.creditapplicationsystem.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,7 +11,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByIdentityNumber(String identityNumber);
     boolean existsByPhoneNumber(String phoneNumber);
 
-    //@Query(value = "Select a.id from applications as a "
-           // + "where a.customer.id =:id" )
+    @Query(value = "Select a.id from applications as a "
+           + "where a.customer.id =:id", nativeQuery = true)
     Long findCustomerByApplicationId(Long id);
 }
