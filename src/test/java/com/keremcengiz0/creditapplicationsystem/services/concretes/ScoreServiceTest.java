a2b4f704e3c1b126499c05b837817742743f71c4
keremcengiz0 <keremcengiz0@hotmail.com>
2023-02-14 15:05:36 +0300

    changed imports for ScoreServiceTest.

--- src/test/java/com/keremcengiz0/creditapplicationsystem/services/concretes/ScoreServiceTest.java
+++ src/test/java/com/keremcengiz0/creditapplicationsystem/services/concretes/ScoreServiceTest.java
@@ -1,18 +1,11 @@
 package com.keremcengiz0.creditapplicationsystem.services.concretes;
 
-import com.keremcengiz0.creditapplicationsystem.dtos.CustomerDTO;
-import com.keremcengiz0.creditapplicationsystem.utils.ScoreServiceTestDataFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
