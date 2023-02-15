e1119c4e5c8d617c7c848b1c776e42256ffed092
keremcengiz0 <keremcengiz0@hotmail.com>
2023-02-15 16:10:40 +0300

    added makeAnApplication_WhenScoreBetween500And1000AndSalarySmallerThan5000_ThenReturnConfirmedApplication, makeAnApplication_WhenScoreBetween500And1000AndSalaryBetween5000And10000_ThenReturnConfirmedApplication, makeAnApplication_WhenScoreBetween500And1000AndSalaryOver10000_ThenReturnConfirmedApplication, makeAnApplication_WhenScoreOver1000_ThenReturnConfirmedApplication, makeAnApplication_WhenCustomerIsNull_ThenThrowUserNotFoundException, getAllApplication_WhenProperInputIsGiven_ThenShouldReturnApplicationDTOList tests.

--- src/main/java/com/keremcengiz0/creditapplicationsystem/mappers/ApplicationMapper.java
+++ src/main/java/com/keremcengiz0/creditapplicationsystem/mappers/ApplicationMapper.java
@@ -18,4 +18,6 @@ public interface ApplicationMapper {
 
     List<ApplicationDTO> fromApplicationListToApplicationDtoList(List<Application> applications);
 
+    List<Application> fromApplicationDtoListToApplicationList(List<ApplicationDTO> applicationDTOS);
+
 }
